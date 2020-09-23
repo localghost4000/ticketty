f4306be04205a96c62f05f0007f4e7f0781b2aa3
lxmrc <19227912+lxmrc@users.noreply.github.com>
2020-09-23 14:35:28 +1000

    Add title functionality for project show page

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -7,9 +7,15 @@ RSpec.feature "Users can create new projects" do
     click_link "New Project"
 
     fill_in "Name", with: "Ruby on Rails"
-    fill_in "Description", with: "Web application framework."
+    fill_in "Description", with: "Like regular Ruby but it's on rails."
     click_button "Create Project"
 
     expect(page).to have_content "Project has been created."
+
+    project = Project.find_by!(name: "Ruby on Rails")
+    expect(page.current_url).to eq project_url(project)
+
+    title = "Ruby on Rails - Projects - Ticketty"
+    expect(page).to have_title title
   end
 end
