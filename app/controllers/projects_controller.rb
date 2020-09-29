4b78ffbdefb2c115bbecdd733d0ea0d61873b73c
lxmrc <19227912+lxmrc@users.noreply.github.com>
2020-09-29 16:11:07 +1000

    Restrict project creation and deletion to admin users

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,25 +1,11 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
 
   def show
-  end
-
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(project_params)
-    
-    if @project.save
-      redirect_to @project, notice: "Project has been created."
-    else
-      flash.now[:alert] = "Couldn't create project."
-      render "new"
-    end
   end
 
   def edit
@@ -33,13 +19,6 @@ class ProjectsController < ApplicationController
       flash.now[:alert] = "Couldn't update project."
       render "edit"
     end
-  end
-
-  def destroy
-    @project.destroy
-
-    flash[:notice] = "Project has been deleted."
-    redirect_to projects_path
   end
 
   private
