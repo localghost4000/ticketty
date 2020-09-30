352ba9a4053e3fd1e90dba697d01d9f08466171e
lxmrc <19227912+lxmrc@users.noreply.github.com>
2020-09-30 12:21:44 +1000

    Add user creation through admin backend

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'application#index'
+
     resources :projects, only: [:new, :create, :destroy]
+    resources :users
   end
 
   resources :projects, only: [:index, :show, :edit, :update] do
